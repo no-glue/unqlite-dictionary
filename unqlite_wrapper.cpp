c1bc5c21b5e8759254ca223064ba1c443cbae987
no-glue <nikola_pav@eunet.rs>
2014-10-17 16:13:50 +0200

    add tokens when inserting key val

--- unqlite_wrapper.cpp
+++ unqlite_wrapper.cpp
@@ -1,9 +1,20 @@
 #include <string>
+#include <iostream>
 #include "unqlite_wrapper.h"
 
 using namespace std;
 
 int main() {
   UnqliteWrapper<string> * table = new UnqliteWrapper<string>();
+  char * result;
+  table->insert("a", "a");
+  table->insert("a", "b");
+  table->insert("a", "c");
+  table->insert("a", "dd");
+  table->insert("a", "ee");
+  result = new char[table->value_size("a")];
+  table->find("a", result);
+  cout<<"a "<<result<<endl;
   delete table;
+  delete result;
 };
--- unqlite_wrapper.h
+++ unqlite_wrapper.h
@@ -8,9 +8,25 @@ public:
   UnqliteWrapper() {open(db);}
   ~UnqliteWrapper() {close(db);}
   void insert(Type key, Type value) {insert(key, value, db);}
+  void find(Type key, char * result) {find(key, result, db);}
+  int value_size(Type key) {return value_size(key, db);}
 private:
   unqlite * db;
   void open(unqlite * & db) {unqlite_open(&db, ":mem:", UNQLITE_OPEN_CREATE);}
   void close(unqlite * & db) {unqlite_close(db);}
-  void insert(Type key, Type value, unqlite * & db) {unqlite_kv_append(db, key.c_str(), -1, value.c_str(), value.length());}
+  void insert(Type key, Type value, unqlite * & db) {unqlite_kv_append(db, key.c_str(), -1, (value + "|").c_str(), (value + "|").length());}
+  void find(Type key, char * result, unqlite * & db) {
+    size_t size;
+    int status;
+    status = unqlite_kv_fetch(db, key.c_str(), -1, NULL, &size);
+    if(status != UNQLITE_OK) return;
+    status = unqlite_kv_fetch(db, key.c_str(), -1, result, &size);
+  }
+  int value_size(Type key, unqlite * & db) {
+    size_t size;
+    int status;
+    status = unqlite_kv_fetch(db, key.c_str(), -1, NULL, &size);
+    if(status != UNQLITE_OK) return 0;
+    return size;
+  }
 };
