3b89727ae04991057949c53d9d7681ef9da01014
no-glue <nikola_pav@eunet.rs>
2014-10-18 18:21:14 +0200

    vars for visited table

--- importer.cpp
+++ importer.cpp
@@ -14,5 +14,56 @@
 using namespace std;
 
 int main() {
+  DoubleList<DoubleNode<string>, string> * results = new DoubleList<DoubleNode<string>, string>();
+  Tokenizer * tokenizer = new Tokenizer(), * tokenizer_visited = new Tokenizer();
+  // tokenizer
+  CstringWrapper * str = new CstringWrapper(), * str_visited = new CstringWrapper();
+  // str for c strings
+  TokenizerList<
+    string,
+    CstringWrapper,
+    DoubleList<DoubleNode<string>, string>,
+    Tokenizer
+  > * tokenizer_list = new TokenizerList<
+    string,
+    CstringWrapper,
+    DoubleList<DoubleNode<string>, string>,
+    Tokenizer
+  >(str, tokenizer);
+  // tokenizer to work with list or something else
+  UnqliteWrapper<string> * table_base = new UnqliteWrapper<string>();
+  // table for storing things
+  HelperWrapper<
+    string,
+    DoubleList<DoubleNode<string>, string>,
+    Tokenizer,
+    TokenizerList<
+      string,
+      CstringWrapper,
+      DoubleList<DoubleNode<string>, string>,
+      Tokenizer
+    >,
+    UnqliteWrapper<string>
+  > * table = new HelperWrapper<
+    string,
+    DoubleList<DoubleNode<string>, string>,
+    Tokenizer,
+    TokenizerList<
+      string,
+      CstringWrapper,
+      DoubleList<DoubleNode<string>, string>,
+      Tokenizer
+    >,
+    UnqliteWrapper<string>
+  >(tokenizer, tokenizer_list, table_base);
+  // table store
+  delete results;
+  delete tokenizer;
+  delete tokenizer_visited;
+  delete str;
+  delete str_visited;
+  delete tokenizer_list;
+  delete table_base;
+  delete table;
   return 0;
 }
