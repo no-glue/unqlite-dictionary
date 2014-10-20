0c0cbe0c152fa1b83d2c354f42de3b5d8dc36956
no-glue <nikola_pav@eunet.rs>
2014-10-20 02:55:56 +0200

    removed importer

--- make.sh
+++ make.sh
@@ -1,4 +1,3 @@
 g++ -Wall -W -fpermissive -std=c++11 unqlite_wrapper.cpp -o unqlite_wrapper -l unqlite
 g++ -Wall -W -fpermissive -std=c++11 sample.cpp -o sample -l unqlite
 g++ -Wall -W -fpermissive -std=c++11 helper_wrapper.cpp -o helper_wrapper -l unqlite
-g++ -Wall -W -fpermissive -std=c++11 importer.cpp -o importer -l unqlite
