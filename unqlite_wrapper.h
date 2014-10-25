296ea2418bd0af47a2a50d226a868e97a7f3c069
no-glue <nikola_pav@eunet.rs>
2014-10-25 16:15:04 +0200

    longest chain

--- unqlite_wrapper.h
+++ unqlite_wrapper.h
@@ -5,20 +5,18 @@ extern "C"
 
 template<typename Type>class UnqliteWrapper {
 public:
-  UnqliteWrapper() {open(db);}
+  UnqliteWrapper():longest_chain(0) {open(db);}
   ~UnqliteWrapper() {close(db);}
   void insert(Type key, Type value) {insert(key, value, db);}
   bool insert_unique(Type key, Type value) {return insert_unique(key, value, db);}
   bool exists(Type key) {return exists(key, db);}
-  void find(Type key, char * result) {find(key, result, db);}
+  void find(Type key, char * result) {find(key, result, db, longest_chain);}
   void remove(Type key) {remove(key, db);}
   int value_size(Type key) {return value_size(key, db);}
-  unsigned int get_longest_chain() {
-    // todo something meaningful here
-    return 0;
-  }
+  unsigned int get_longest_chain() {return longest_chain;}
 private:
   unqlite * db;
+  unsigned int longest_chain;
   void open(unqlite * & db) {unqlite_open(&db, ":mem:", UNQLITE_OPEN_CREATE);}
   void close(unqlite * & db) {unqlite_close(db);}
   void insert(Type key, Type value, unqlite * & db) {unqlite_kv_append(db, key.c_str(), -1, (value + "|").c_str(), (value + "|").length());}
@@ -35,11 +33,12 @@ private:
     status = unqlite_kv_fetch(db, key.c_str(), -1, NULL, &size);
     return (status != UNQLITE_OK || !size) ? false : true;
   }
-  void find(Type key, char * result, unqlite * & db) {
+  void find(Type key, char * result, unqlite * & db, unsigned int & longest_chain) {
     // find value
     size_t size;
     int status;
     status = unqlite_kv_fetch(db, key.c_str(), -1, NULL, &size);
+    longest_chain = size;
     if(status != UNQLITE_OK) return;
     status = unqlite_kv_fetch(db, key.c_str(), -1, result, &size);
   }
